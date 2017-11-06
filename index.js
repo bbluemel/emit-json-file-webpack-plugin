e9c17f4cb4dc2d6da4f4ec45c1df3857dab7d8b4
Ben Bluemel <ben.bluemel@mso.net>
2017-11-06 13:56:21 +0000

    Allow dynamic files and associating with other files
    e.g. Can use info from postcss-modules's getJSON to export the json to be stored alongside the CSS

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 const path = require('path')
 const jsonFormat = require('json-format')
-
+const loaderUtils = require('loader-utils')
 class EmitJsonFileWebpackPlugin {
-	constructor(options) {
-    if (!Array.isArray(options)) {
-      throw new Error('[emit-json-file-webpack-plugin] arguments must be an array')
+	constructor(files, options) {
+
+    if (!Array.isArray(files) && typeof files !== 'function') {
+      throw new Error('[emit-json-file-webpack-plugin] arguments must be an array or function that returns an array')
     }
-		this.options = options || []
+		this.files = files || []
+    this.options = options || {}
 	}
 
 	apply(compiler) {
-    const options = this.options
+    const files = ( typeof this.files === 'function' ) ? this.files() : this.files
+    const filename = this.options.filename || '[name].json'
     compiler.plugin('emit', function(compilation, cb) {
-      options.forEach(file => {
-        if (!file.content || !file.path) {
-          compilation.errors.push(new Error(`[emit-json-file-webpack-plugin] file arguments must contain 'path' and 'content'`))
+
+      files.forEach(file => {
+        if (!file.content || (!file.path && !file.linkedTo)) {
+          compilation.errors.push(new Error(`[emit-json-file-webpack-plugin] file arguments must contain 'content' and either 'path' or 'linkedTo'`))
           return
         }
         const formatted = jsonFormat(file.content)
+        const linkedTo = file.linkedTo || false;
+        if ( linkedTo ) {
+          compilation.chunks.forEach( chunk => {
+            chunk.forEachModule(function(module) {
+              module.fileDependencies.forEach(function(filepath) {
+                if ( filepath == linkedTo ) {
+                  const getPath = format => compilation.getPath(format, {
+                    chunk,
+                  }).replace(/\[(?:(\w+):)?contenthash(?::([a-z]+\d*))?(?::(\d+))?\]/ig, function () { 
+                    return loaderUtils.getHashDigest(formatted, arguments[1], arguments[2], parseInt(arguments[3], 10));
+                  })
+                  file.path = getPath(filename)
+                  if (!chunk.files.includes(file.path)) {
+                    chunk.files.push(file.path)
+                  }
+                }
+
+              })
+            })
+          })
+        }
+        
         compilation.assets[file.path] = {
           size: () => formatted.length,
           source: () => formatted
